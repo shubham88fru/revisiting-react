10aaa7fa35733cf5fa1435760f7f97b5217f7bce
shubham88fru <shubham88fru@yahoo.com>
2024-06-25 22:04:36 -0500

    add useReducer hook

--- react-quiz/src/App.js
+++ react-quiz/src/App.js
@@ -1,8 +1,50 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading", //loading, error, ready, active, finished
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        status: "ready",
+        question: action.payload,
+      };
+
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) =>
+        dispatch({
+          type: "dataReceived",
+          payload: data,
+        })
+      )
+      .catch((err) =>
+        dispatch({
+          type: "dataFailed",
+        })
+      );
+  }, []);
+
   return (
     <div className="app">
       <Header />
