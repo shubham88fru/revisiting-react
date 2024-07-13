30ae20aa58f398cdc1f2fee5c5fc808335b8339a
shubham88fru <shubham88fru@yahoo.com>
2024-07-13 15:12:57 -0500

    lot of stuff

--- worldwise/src/App.jsx
+++ worldwise/src/App.jsx
@@ -7,6 +7,8 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
+import CountriesList from "./components/CountriesList";
+import City from "./components/City";
 
 const BASE_URL = `http://localhost:9000`;
 
@@ -46,7 +48,11 @@ function App() {
             path="cities"
             element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="cities/:id" element={<City />} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p>Form</p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
---
+++ worldwise/src/components/CountriesList.jsx
@@ -0,0 +1,29 @@
+import styles from "./CountryList.module.css";
+import CountryItem from "./CountryItem";
+import Spinner from "./Spinner";
+import Message from "./Message";
+
+function CountriesList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking on a city on the map" />
+    );
+
+  const countries = cities.reduce((acc, curr) => {
+    if (!acc.map((el) => el.country).includes(curr.country)) {
+      return [...acc, { country: curr.country, emoji: curr.emoji }];
+    } else return acc;
+  }, []);
+
+  return (
+    <ul className={styles.countryList}>
+      {countries.map((country) => (
+        <CountryItem id={country} country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
