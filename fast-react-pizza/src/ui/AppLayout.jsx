a231b62febbd9935a668673d5ac32992963fe0b9
shubham88fru <shubham88fru@yahoo.com>
2024-08-10 14:34:00 -0500

    react router actions

--- fast-react-pizza/src/ui/AppLayout.jsx
+++ fast-react-pizza/src/ui/AppLayout.jsx
@@ -5,7 +5,7 @@ import Loader from "./Loader";
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === "loading"; //idle, loading, submitting.
 
   return (
     <div className="layout">
