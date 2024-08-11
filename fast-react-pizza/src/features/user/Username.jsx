28881749bd883ccd169dabbb30ee7d4704fdf031
shubham88fru <shubham88fru@yahoo.com>
2024-08-11 15:04:58 -0500

    apply styling

---
+++ fast-react-pizza/src/features/user/Username.jsx
@@ -0,0 +1,5 @@
+function Username() {
+  return <div className="text-sm font-semibold hidden">Shubham</div>;
+}
+
+export default Username;
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header>
-      <Link to="/">Fast React Pizza Co.</Link>
+    <header className="bg-yellow-500 uppercase px-4 py-3 border-b border-stone-200">
+      <Link className="tracking-widest" to="/">
+        Fast React Pizza Co.
+      </Link>
       <SearchOrder />
-      <p>Shubham</p>
+      <Username />
     </header>
   );
 }
