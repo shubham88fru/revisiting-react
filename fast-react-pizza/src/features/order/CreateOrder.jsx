f0adb64264cb85602e5900030dbd78471c00f88d
shubham88fru <shubham88fru@yahoo.com>
2024-08-15 20:03:56 -0500

    style

--- fast-react-pizza/src/features/order/CreateOrder.jsx
+++ fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -77,7 +77,10 @@ function CreateOrder() {
 
         <div>
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <button disabled={isSubmitting}>
+          <button
+            disabled={isSubmitting}
+            className="bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block tracking-wide rounded-full hover:bg-yellow-300 transition-colors duration-3 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed"
+          >
             {isSubmitting ? "Placing order..." : "Order now"}
           </button>
         </div>
--- fast-react-pizza/src/ui/Header.jsx
+++ fast-react-pizza/src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from "../features/user/Username";
 
 function Header() {
   return (
-    <header className="flex items-center justify-between bg-yellow-500 uppercase sm:px-6 px-4 py-3 border-b border-stone-200">
+    <header className="flex items-center justify-between bg-yellow-400 uppercase sm:px-6 px-4 py-3 border-b border-stone-200">
       <Link className="tracking-widest" to="/">
         Fast React Pizza Co.
       </Link>
