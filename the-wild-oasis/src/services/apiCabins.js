5c135fe071a1582ae4fb5634054eeccd76e52f6a
shubham88fru <shubham88fru@yahoo.com>
2024-10-03 17:36:49 -0700

    add image upload feture

--- the-wild-oasis/src/services/apiCabins.js
+++ the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
   const { data, error } = await supabase.from("cabins").select("*");
@@ -11,14 +11,35 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    "/",
+    ""
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
   const { data, error } = await supabase
     .from("cabins")
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.log(error);
     throw new Error("Cabins could not be created..");
+  }
+
+  //upload the image to bucket.
+  const { error: storageError } = await supabase.storage
+    .from("cabin-images")
+    .upload(imageName, newCabin.image);
+
+  //Delete the cabin if there was an error uploading images.
+  if (storageError) {
+    await supabase.from("cabins").delete().eq("id", data.id);
+
+    console.error(storageError);
+    throw new Error(
+      "Cabin image could not be uploaded and the cabin wasn't created."
+    );
   }
 
   return data;
--- the-wild-oasis/src/services/supabase.js
+++ the-wild-oasis/src/services/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://wjgldbngaxfwacpqquyw.supabase.co";
+export const supabaseUrl = "https://wjgldbngaxfwacpqquyw.supabase.co";
 const supabaseKey = //not dangerous to do this.
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndqZ2xkYm5nYXhmd2FjcHFxdXl3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcyMTM1MDMsImV4cCI6MjA0Mjc4OTUwM30.DMctVrjvrz76AD8KJAodu-8dPZVdiJ6ylvsOK9WR3Rk";
 const supabase = createClient(supabaseUrl, supabaseKey);
