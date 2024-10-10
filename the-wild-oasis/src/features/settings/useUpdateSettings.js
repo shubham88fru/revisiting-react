097c5f22aad48a3165e51833b64a9773f7338a9d
shubham88fru <shubham88fru@yahoo.com>
2024-10-09 18:14:25 -0700

    integrate the update settings api

---
+++ the-wild-oasis/src/features/settings/useUpdateSettings.js
@@ -0,0 +1,22 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { updateSetting as updateSettingAPI } from "../../services/apiSettings";
+
+export function useUpdateSettings() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingAPI,
+    onSuccess: () => {
+      toast.success("Setting successfully edited..");
+      queryClient.invalidateQueries({
+        queryKey: ["settings"],
+      });
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+  });
+
+  return { isUpdating, updateSetting };
+}
