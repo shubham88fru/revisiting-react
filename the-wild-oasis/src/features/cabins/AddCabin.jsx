bd05b3ce4b9f5d3f9c51e0fc86f603910a71031b
shubham88fru <shubham88fru@yahoo.com>
2024-10-17 16:14:23 -0700

    implement modal as a compound component

--- the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
+import CabinTable from "./CabinTable";
 
 function AddCabin() {
-  const [isOpenModel, setIsOpenModel] = useState(false);
-
   return (
-    <div>
-      <Button onClick={() => setIsOpenModel((show) => !show)}>
-        Add new cabin
-      </Button>
-      {isOpenModel && (
-        <Modal onClose={() => setIsOpenModel(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModel(false)} />
-        </Modal>
-      )}
-    </div>
+    <Modal>
+      <Modal.Open opens="cabin-form">
+        <Button>Add new Cabin</Button>
+      </Modal.Open>
+      <Modal.Window name="cabin-form">
+        <CreateCabinForm />
+      </Modal.Window>
+
+      <Modal.Open opens="table">
+        <Button>Show tables</Button>
+      </Modal.Open>
+      <Modal.Window name="table">
+        <CabinTable />
+      </Modal.Window>
+    </Modal>
   );
 }
 
