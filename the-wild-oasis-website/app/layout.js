842435207a6e6248eb7c8ca3b7b317666a85ca70
shubham88fru <shubham88fru@yahoo.com>
2024-10-26 15:29:03 -0700

    tailwind setup

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -1,5 +1,6 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+import "@/app/_styles/globals.css";
 
 export const metadata = {
   title: "The Wild Oasis", //page title.
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-blue-800">
         <header>
           <Logo />
           <Navigation />
