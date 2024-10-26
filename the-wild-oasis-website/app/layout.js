d98eee91e8cc114d3983cb4cf87b99758c5ef246
shubham88fru <shubham88fru@yahoo.com>
2024-10-26 15:42:45 -0700

    metadata and more

--- the-wild-oasis-website/app/layout.js
+++ the-wild-oasis-website/app/layout.js
@@ -3,7 +3,15 @@ import Navigation from "@/app/_components/Navigation";
 import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis", //page title.
+  // title: "The Wild Oasis", //page title.
+  title: {
+    //page title.
+    template: "%s The Wild Oasis",
+    default: "Welcome / The Wild Oasis",
+  },
+  //description of the page (for SEO, traditionally put in the meta tag.)
+  description:
+    "Luxurious cabin hotel, located in the heart of the italian Dolomites, surrounded by beautiful mountains and dark forests.",
 };
 
 export default function RootLayout({ children }) {
