5a0264a5ec21e9a25a0af1dbf2976e8a67b69043
shubham88fru <shubham88fru@yahoo.com>
2024-12-15 12:56:58 -0700

    custom signin button

--- the-wild-oasis-website/app/account/page.js
+++ the-wild-oasis-website/app/account/page.js
@@ -1,9 +1,16 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-function Page() {
-  return <div>Welcome, Shubham</div>;
+async function Page() {
+  const session = await auth();
+  return (
+    <div className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome, {session.user.name.split(" ")[0]}
+    </div>
+  );
 }
 
 export default Page;
