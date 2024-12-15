6a96064596e1a276754edccd893c7ea23e32e073
shubham88fru <shubham88fru@yahoo.com>
2024-12-15 13:18:51 -0700

    create users

--- the-wild-oasis-website/app/_lib/auth.js
+++ the-wild-oasis-website/app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import { createGuest, getGuest } from "./data-service";
 
 const authConfig = {
   secret: process.env.NEXT_PUBLIC_SECRET,
@@ -12,6 +13,21 @@ const authConfig = {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth?.user;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest)
+          await createGuest({ email: user.email, fullName: user.name });
+        return true;
+      } catch (err) {
+        return false;
+      }
+    },
+    async session({ session, user }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {
