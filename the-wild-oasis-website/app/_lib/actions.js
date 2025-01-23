6f19a2d2066ae20b9442e19bc499376cf7f0b6f4
shubham88fru <shubham88fru@yahoo.com>
2025-01-23 16:34:40 -0700

    delete a reservationg

--- the-wild-oasis-website/app/_lib/actions.js
+++ the-wild-oasis-website/app/_lib/actions.js
@@ -3,6 +3,7 @@
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import supabase from "./supabase";
+import { getBookings } from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -29,6 +30,28 @@ export async function updateGuest(formData) {
   revalidatePath("/account/profile");
 
   return data;
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in.");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const { error } = await supabase
+    .from("bookings")
+    .delete()
+    .eq("id", bookingId);
+
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+  if (!guestBookingIds.includes(bookingId)) {
+    throw new Error("You are not authorized to delete this reservation.");
+  }
+
+  if (error) {
+    throw new Error("Reservation could not be deleted");
+  }
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
--- the-wild-oasis-website/app/account/reservations/page.js
+++ the-wild-oasis-website/app/account/reservations/page.js
@@ -1,4 +1,6 @@
 import ReservationCard from "@/app/_components/ReservationCard";
+import { auth } from "@/app/_lib/auth";
+import { getBookings } from "@/app/_lib/data-service";
 
 export const metadata = {
   title: "Reservations",
